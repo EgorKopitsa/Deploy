7cc24e897ceadd41ca10fad32d5b17a9d3c6e762
EgorKopitsa <kopitsa.egor.2020@gmail.com>
2020-11-16 17:37:25 +0300

    21

--- Shop/settings.py
+++ Shop/settings.py
@@ -82,24 +82,24 @@ WSGI_APPLICATION = 'Shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shopbd',
+        'USER': 'albmont_admin',
+        'PASSWORD': 'rbnvjrnvoprentovr',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'shopbd',
-            'USER': 'albmont_admin',
-            'PASSWORD': 'rbnvjrnvoprentovr',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 
 # Password validation
