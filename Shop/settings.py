b44c8526c8d4a05e6e2373931b479e8ad7870071
EgorKopitsa <kopitsa.egor.2020@gmail.com>
2020-11-18 17:16:24 +0300

    38

--- Shop/settings.py
+++ Shop/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
