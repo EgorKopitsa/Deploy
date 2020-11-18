c7e1cdc79ad29ec921ca7e1d730cd31d3a08df17
EgorKopitsa <kopitsa.egor.2020@gmail.com>
2020-11-18 15:09:10 +0300

    32

--- Shop/settings.py
+++ Shop/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/egor/shop/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
