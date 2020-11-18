67b60be0aa4a827949b754309f5422680893ef76
EgorKopitsa <kopitsa.egor.2020@gmail.com>
2020-11-18 18:10:53 +0300

    40

--- Shop/settings.py
+++ Shop/settings.py
@@ -137,10 +137,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
